dc70b9672df04e253c54fabe9b15b5458c850d57
HenzelMoras <henzelmoras@gmail.com>
2021-07-01 15:33:04 +0530

    update nvim-config-setup :green_book:

--- nvim-config.sh
+++ nvim-config.sh
@@ -6,11 +6,26 @@ sudo apt-get install gettext libtool libtool-bin autoconf automake cmake g++ pkg
 cd $(mktemp -d)
 git clone https://github.com/neovim/neovim --depth 1
 cd neovim
-sudo checkinstall make CMAKE_BUILD_TYPE=Release install
+sudo checkinstall -y make CMAKE_BUILD_TYPE=Release install
 cd ..
 sudo rm -r neovim
 
 # install lunarvim
+
+# backup nvim config
+mv ~/.config/nvim ~/.config/nvim.bak
+
+sudo apt install -y xclip python3-pip nodejs npm ripgrep fzf ranger libjpeg8-dev zlib1g-dev python-dev python3-dev libxtst-dev python3-pip
+
+sudo npm install -g tree-sitter-cli
+
+pip3 install ueberzug neovim neovim-remote pynvim --user
+
 bash <(curl -s https://raw.githubusercontent.com/ChristianChiarulli/lunarvim/master/utils/installer/install.sh)
+
+
+# The output of version information should include a line for: LuaJIT 
+nvim -v
+
 
 #Done
